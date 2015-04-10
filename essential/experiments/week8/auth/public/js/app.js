3b4cc4a3a1dc93b538e06a807b8f7550bd11c0db
songchitara <willcxong@gmail.com>
2015-04-10 19:18:20 -0400

    add auth folder to week8

---
+++ essential/experiments/week8/auth/public/js/app.js
@@ -0,0 +1,76 @@
+﻿var app = angular.module("PassportApp", ["ngRoute"]);
+
+
+
+
+app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home/home.html'
+        })
+        .when('/profile', {
+            templateUrl: 'views/profile/profile.html',
+            resolve : {
+	            loggedin: checkLoggedin
+            }
+        })
+        .when('/login', {
+            templateUrl: 'views/login/login.html',
+            controller: 'LoginCtrl'
+        })
+        .when('/register', {
+            templateUrl: 'views/register/register.html',
+            controller: 'RegisterCtrl'
+        })
+
+    }]);
+    
+    
+    
+var checkLoggedin = function($q, $timeout, $http, $location, $rootScope)
+{
+    var deferred = $q.defer();
+
+    $http.get('/loggedin').success(function(user)
+    {
+        $rootScope.errorMessage = null;
+        // User is Authenticated
+        if (user !== '0')
+        {
+            $rootScope.currentUser = user;
+            deferred.resolve();
+        }
+        // User is Not Authenticated
+        else
+        {
+            $rootScope.errorMessage = 'You need to log in.';
+            deferred.reject();
+            $location.url('/login');
+        }
+    });
+    
+    return deferred.promise;
+};
+
+    
+    
+app.controller("NavCtrl", function($scope, $http, $location){
+	$scope.logout = function() {
+		$http.post('/logout')
+		.success(function(response){
+			$location.url('/home');
+			console.log("logged out! " + response);
+		})
+	}
+});    
+
+
+// Debug purpose: view all the users
+// $http.post("/rest/user")
+// .success(function (response) {
+//     console.log('success debug');
+// });
+    
+    
+    
+ 
---
+++ essential/experiments/week8/auth/public/views/register/register.js
@@ -0,0 +1,34 @@
+﻿app.controller("RegisterCtrl", function ($scope, $http, $rootScope, $location) {
+    $scope.register = function (user) {
+        console.log(user);
+        //
+        if(user.password == user.password2) {
+	        $http.post("/register", user)
+	        .success(function (response) {
+	            console.log(response);
+                // response is false when user already exists.
+                if (response==false) {
+                    alert('User already exists!');
+                }
+	            $rootScope.currentUser = response;
+	            $location.url("/profile");
+        	})	
+        }
+        else{
+            alert('Two passwords not match! Please retry.')
+        }
+    }
+
+    // check if two typed passwords are the same 
+    $scope.checkPwds = function () {
+        if ($scope.user) {
+            if ($scope.user.password && $scope.user.password2) {
+                if ($scope.user.password != $scope.user.password2) {
+                    return 'Passwords do not match!';
+                } 
+            }
+            
+        }   
+            
+    }
+})
